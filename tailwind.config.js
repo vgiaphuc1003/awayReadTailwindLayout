421a3ef45bfd6e186fcf704be2da16c17c2b2177
vgiaphuc1003 <101577074+vgiaphuc1003@users.noreply.github.com>
2023-11-23 14:51:36 +0700

    Update Config CSS
    Update config CSS and Add variable CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/components/**/*.{html,js}"],
-    theme: {
-        extend: {
-            colors: {
-                primary: "rgb(var(--color-primary) / <alpha-value>)",
-                second: "rgb(var(--color-second) / <alpha-value>)",
-                bgcolor: "rgb(var(--color-bgcolor) / <alpha-value>)",
-                btn: "rgb(var(--color-btn) / <alpha-value>)",
-                blue: "rgb(var(--color-blue) / <alpha-value>)",
-            },
-        },
+  content: ["./src/components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "rgb(var(--color-white) / <alpha-value>)",
+        black: "rgb(var(--color-black) / <alpha-value>)",
+        gray: "rgb(var(--color-gray) / <alpha-value>)",
+        graydarker: "rgb(var(--color-graydarker) / <alpha-value>)",
+        blue: "rgb(var(--color-blue) / <alpha-value>)",
+        bluedarker: "rgb(var(--color-bluedarker) / <alpha-value>)",
+        yellow: "rgb(var(--color-yellow) / <alpha-value>)",
+        yellowdarker: "rgb(var(--color-yellowdarker) / <alpha-value>)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
